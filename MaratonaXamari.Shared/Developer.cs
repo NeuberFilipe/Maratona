2af50dc509c60023931a7ef90c7024ccd55d0213
Neuber Filipe Santana Coelho <neuber.coelho@groupsoftware.com.br>
2017-05-10 11:59:20 -0300

    Your Message

---
+++ MaratonaXamari.Shared/Developer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaratonaXamari.Shared
+{
+   public class Developer
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("cidade")]
+        public string Cidade { get; set; }
+
+        [JsonProperty("estado")]
+        public string Estado { get; set; }
+    }
+}
---
+++ MaratonaXamarin.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using MaratonaXamari.Shared;
+using System;
+using System.Linq;
+
+namespace MaratonaXamarin.Android
+{
+    [Activity(Label = "MaratonaXamarin.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+             SetContentView (Resource.Layout.Main);
+            var button = this.FindViewById<Button>(Resource.Id.btnCarregar);
+            var listView = this.FindViewById<ListView>(Resource.Id.lvwItens);
+
+            button.Click += async (sender, e) =>
+            {
+                var api = new UserApi();
+                var users = await api.ListAsync(new Developer
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Neuber",
+                    Email = "bnksystem@outlook.com",
+                    Estado = "MG",
+                    Cidade = "BETIM"
+                });
+
+                listView.Adapter = new ArrayAdapter(this,
+                                                    Android.Resource.Layout.Main,
+                                                         users.OrderBy(y => y.Name).Select(x => $"{x.Id} {x.Name}").ToArray());
+            };
+        }
+    }
+}
+
